66c43af9847be1982d5bc2ddffee26d80e67dc32
Michalik, Joshua <jsmichal@usa.wtbts.net>
2017-01-25 13:51:44 -0500

    Add jw songs

--- js/audioAssets.js
+++ js/audioAssets.js
@@ -3,12 +3,112 @@
 // Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
 var audioData = [
     {
-        'title' : 'Episode 140',
-        'url' : 'https://feeds.soundcloud.com/stream/275202399-amazon-web-services-306355661-amazon-web-services.mp3'
+      'title': 'blessings of learning a language',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/blessings-of-learning-a-language.mp3'
     },
     {
-        'title' : 'Episode 139',
-        'url' : 'https://feeds.soundcloud.com/stream/274166909-amazon-web-services-306355661-aws-podcast-episode-139.mp3'
+      'title': 'if you could see what i see',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/if-you-could-see-what-i-see.mp3'
+    },
+    {
+      'title': 'now is the time',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/now-is-the-time.mp3'
+    },
+    {
+      'title': 'choices',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/choices.mp3'
+    },
+    {
+      'title': 'i give you my best',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/i-give-you-my-best.mp3'
+    },
+    {
+      'title': 'our thanks go to you',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/our-thanks-go-to-you.mp3'
+    },
+    {
+      'title': 'dont run so fast',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/dont-run-so-fast.mp3'
+    },
+    {
+      'title': 'imagine the time',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/imagine-the-time.mp3'
+    },
+    {
+      'title': 'study makes you strong',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/study-makes-you-strong.mp3'
+    },
+    {
+      'title': 'finding treasures',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/finding-treasures.mp3'
+    },
+    {
+      'title': 'jehovah welcomes you home',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/jehovah-welcomes-you-home.mp3'
+    },
+    {
+      'title': 'take your time choose wisely',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/take-your-time-choose-wisely.mp3'
+    },
+    {
+      'title': 'follow the course of hospitality',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/follow-the-course-of-hospitality.mp3'
+    },
+    {
+      'title': 'joy of conventions',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/joy-of-conventions.mp3'
+    },
+    {
+      'title': 'the best life ever',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/the-best-life-ever.mp3'
+    },
+    {
+      'title': 'forgive one another',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/forgive-one-another.mp3'
+    },
+    {
+      'title': 'just around the corner',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/just-around-the-corner.mp3'
+    },
+    {
+      'title': 'we wont forget you',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/we-wont-forget-you.mp3'
+    },
+    {
+      'title': 'give you my all',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/give-you-my-all.mp3'
+    },
+    {
+      'title': 'just a smile',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/just-a-smile.mp3'
+    },
+    {
+      'title': 'what means the most to me',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/what-means-the-most-to-me.mp3'
+    },
+    {
+      'title': 'glad i passed the test',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/glad-i-passed-the-test.mp3'
+    },
+    {
+      'title': 'keep the pace',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/keep-the-pace.mp3'
+    },
+    {
+      'title': 'where i belong',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/where-i-belong.mp3'
+    },
+    {
+      'title': 'honor jehovah with your valuable things',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/honor-jehovah-with-your-valuable-things.mp3'
+    },
+    {
+      'title': 'never give up',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/never-give-up.mp3'
+    },
+    {
+      'title': 'your word endures forever',
+      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/your-word-endures-forever.mp3'
     }
 ];
 
