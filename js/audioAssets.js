1344b790ad7c97643b2999ccdfd2b2f47ecc2bfa
Michalik, Joshua <jsmichal@usa.wtbts.net>
2017-01-27 13:36:55 -0500

    Change urls to be amazon

--- js/audioAssets.js
+++ js/audioAssets.js
@@ -4,111 +4,111 @@
 var audioData = [
     {
       'title': 'blessings of learning a language',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/blessings-of-learning-a-language.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/blessings-of-learning-a-language.mp3'
     },
     {
       'title': 'if you could see what i see',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/if-you-could-see-what-i-see.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/if-you-could-see-what-i-see.mp3'
     },
     {
       'title': 'now is the time',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/now-is-the-time.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/now-is-the-time.mp3'
     },
     {
       'title': 'choices',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/choices.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/choices.mp3'
     },
     {
       'title': 'i give you my best',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/i-give-you-my-best.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/i-give-you-my-best.mp3'
     },
     {
       'title': 'our thanks go to you',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/our-thanks-go-to-you.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/our-thanks-go-to-you.mp3'
     },
     {
       'title': 'dont run so fast',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/dont-run-so-fast.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/dont-run-so-fast.mp3'
     },
     {
       'title': 'imagine the time',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/imagine-the-time.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/imagine-the-time.mp3'
     },
     {
       'title': 'study makes you strong',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/study-makes-you-strong.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/study-makes-you-strong.mp3'
     },
     {
       'title': 'finding treasures',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/finding-treasures.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/finding-treasures.mp3'
     },
     {
       'title': 'jehovah welcomes you home',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/jehovah-welcomes-you-home.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/jehovah-welcomes-you-home.mp3'
     },
     {
       'title': 'take your time choose wisely',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/take-your-time-choose-wisely.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/take-your-time-choose-wisely.mp3'
     },
     {
       'title': 'follow the course of hospitality',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/follow-the-course-of-hospitality.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/follow-the-course-of-hospitality.mp3'
     },
     {
       'title': 'joy of conventions',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/joy-of-conventions.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/joy-of-conventions.mp3'
     },
     {
       'title': 'the best life ever',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/the-best-life-ever.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/the-best-life-ever.mp3'
     },
     {
       'title': 'forgive one another',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/forgive-one-another.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/forgive-one-another.mp3'
     },
     {
       'title': 'just around the corner',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/just-around-the-corner.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/just-around-the-corner.mp3'
     },
     {
       'title': 'we wont forget you',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/we-wont-forget-you.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/we-wont-forget-you.mp3'
     },
     {
       'title': 'give you my all',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/give-you-my-all.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/give-you-my-all.mp3'
     },
     {
       'title': 'just a smile',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/just-a-smile.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/just-a-smile.mp3'
     },
     {
       'title': 'what means the most to me',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/what-means-the-most-to-me.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/what-means-the-most-to-me.mp3'
     },
     {
       'title': 'glad i passed the test',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/glad-i-passed-the-test.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/glad-i-passed-the-test.mp3'
     },
     {
       'title': 'keep the pace',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/keep-the-pace.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/keep-the-pace.mp3'
     },
     {
       'title': 'where i belong',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/where-i-belong.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/where-i-belong.mp3'
     },
     {
       'title': 'honor jehovah with your valuable things',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/honor-jehovah-with-your-valuable-things.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/honor-jehovah-with-your-valuable-things.mp3'
     },
     {
       'title': 'never give up',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/never-give-up.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/never-give-up.mp3'
     },
     {
       'title': 'your word endures forever',
-      'url': 'https://github.com/lifegems/jwsongs/raw/master/songs/your-word-endures-forever.mp3'
+      'url': 'https://s3.amazonaws.com/jwsongs/your-word-endures-forever.mp3'
     }
 ];
 
